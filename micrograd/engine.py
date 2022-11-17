fix softmax & embedding backward bugs

micrograd/engine.py
@@ -29,7 +29,7 @@ class Tensor:
 
         return out
 
-    def backward(self):
+    def backward(self, gradient=None):
         topo = []
         visited = set()
 
@@ -42,7 +42,7 @@ class Tensor:
 
         build_topo(self)
 
-        self.grad = np.ones_like(self.data)
+        self.grad = gradient if gradient is not None else np.ones_like(self.data)
         for node in reversed(topo):
             node._backward()
 
@@ -51,11 +51,19 @@ class Tensor:
 
         def _backward():
             # TODO: make this more robust for different vector / matrix shapes
-            self.grad += out.grad.dot(other.data.T)
-            if out.grad.ndim == 1:
+            if out.grad.ndim == 1:  # vector matrix product
+                self.grad += out.grad.dot(other.data.T)
                 other.grad += self.data[None, ...].T.dot(out.grad[None, ...])
-            else:
+            elif out.grad.ndim == 2:  # standard 2D  matmul
+                self.grad += out.grad.dot(other.data.T)
                 other.grad += self.data.T.dot(out.grad)
+            else:  # batched matmul
+                self.grad += micrograd.unbroadcast(
+                    out.grad @ other.data.swapaxes(-1, -2), self.shape
+                )
+                other.grad += micrograd.unbroadcast(
+                    self.data.swapaxes(-1, -2) @ out.grad, other.shape
+                )
 
         out._backward = _backward
 
@@ -152,12 +160,19 @@ class Tensor:
         out = Tensor(s, (self,), "softmax")
 
         def _backward():
-            a = np.swapaxes(s, dim, -1)
-            local_deriv = micrograd.diag_embed(a).data - (
-                np.expand_dims(a, axis=-2) * np.expand_dims(a, axis=-1)
+            # TODO: there must be a cleaner way?
+            swapped_s = np.swapaxes(s, dim, -1)
+            local_deriv = micrograd.diag_embed(swapped_s).data - (
+                np.expand_dims(swapped_s, axis=-2) * np.expand_dims(swapped_s, axis=-1)
             )
-            local_deriv = np.swapaxes(local_deriv, dim, -1)
-            self.grad = self.grad + out.grad.dot(local_deriv)
+            swapped_out_grad = np.swapaxes(out.grad, dim, -1)
+            reshaped_out_grad = swapped_out_grad.reshape(-1, 1, swapped_s.shape[-1])
+            reshaped_local_deriv = local_deriv.reshape(
+                -1, swapped_s.shape[-1], swapped_s.shape[-1]
+            )
+            res = reshaped_out_grad @ reshaped_local_deriv
+            res = res.reshape(swapped_out_grad.shape).swapaxes(dim, -1)
+            self.grad += res
 
         out._backward = _backward
 

micrograd/functional.py
@@ -27,26 +27,35 @@ def cross_entropy(input: Tensor, target: int):
     return out
 
 
-def batched_cross_entropy(input: Tensor, target: Tensor) -> Tensor:
+def batched_cross_entropy(input: Tensor, target: Tensor, ignore_index=-100) -> Tensor:
     """
     Computes the cross entropy loss between input logits and target
 
     Args:
-        input (micrograd.Tensor) raw logits of shape (N, C)
-        target (np.array) Ground truth class indices of shape (N)
+        input (Tensor) raw logits of shape (N, C)
+        target (Tensor) ground truth class indices of shape (N)
     """
     input_max = input.data.max(axis=-1, keepdims=True)
     softmax_num = np.exp(input.data - input_max)
     softmax_denom = np.sum(softmax_num, axis=-1, keepdims=True)
     probs = softmax_num / softmax_denom
     p = probs[np.arange(probs.shape[0]), target.data]
-    out = Tensor(-np.log(p).mean(keepdims=True), (input,), "cross_entropy")
+
+    dont_ignore_mask = target.data != ignore_index
+    neg_log_probs = -np.log(p)
+    out = Tensor(
+        neg_log_probs.mean(keepdims=True, where=dont_ignore_mask),
+        (input,),
+        "cross_entropy",
+    )
 
     def _backward():
         batch_size = probs.shape[0]
+        num_ignored = batch_size - dont_ignore_mask.sum()
         input.grad = np.copy(probs)
         input.grad[np.arange(batch_size), target.data] -= 1.0
-        input.grad /= batch_size
+        input.grad *= np.expand_dims(dont_ignore_mask, -1)
+        input.grad /= batch_size - num_ignored
 
     out._backward = _backward
 
@@ -143,7 +152,7 @@ def embedding(input: Tensor, weight: Tensor) -> Tensor:
 
     def _backward():
         reshaped_input = input.data.reshape(-1)
-        reshaped_out_grad = out.grad.reshape(-1)
+        reshaped_out_grad = out.grad.reshape(-1, weight.shape[-1])
         # TODO: speed up
         for idx, g in zip(reshaped_input, reshaped_out_grad):
             weight.grad[idx] += g

test/test_engine.py
@@ -274,8 +274,38 @@ def test_sigmoid_backward():
     assert np.allclose(ma.grad, ta.grad.numpy())
 
 
-def test_matmul_foward():
+def test_vector_matmul_forward():
     ma = Tensor(np.random.randn(2))
+    mb = Tensor(np.random.randn(2, 2))
+    mc = ma.matmul(mb)
+
+    ta = torch.from_numpy(ma.data)
+    tb = torch.from_numpy(mb.data)
+    tc = ta @ tb
+
+    assert np.allclose(mc.data, tc.numpy())
+
+
+def test_vector_matmul_backward():
+    ma = Tensor(np.random.randn(2))
+    mb = Tensor(np.random.randn(2, 2))
+    mc = ma.matmul(mb)
+    mc.backward()
+
+    ta = torch.from_numpy(ma.data)
+    ta.requires_grad = True
+    tb = torch.from_numpy(mb.data)
+    tb.requires_grad = True
+    tc = ta @ tb
+    tc.backward(gradient=torch.ones_like(tc))
+
+    assert np.allclose(ma.grad, ta.grad.numpy())
+    assert np.allclose(mb.grad, tb.grad.numpy())
+
+
+def test_matmul_forward():
+    batch_size = 16
+    ma = Tensor(np.random.randn(batch_size, 2))
     mb = Tensor(np.random.randn(2, 2))
     mc = ma.matmul(mb)
 
@@ -287,7 +317,8 @@ def test_matmul_foward():
 
 
 def test_matmul_backward():
-    ma = Tensor(np.random.randn(2))
+    batch_size = 16
+    ma = Tensor(np.random.randn(batch_size, 2))
     mb = Tensor(np.random.randn(2, 2))
     mc = ma.matmul(mb)
     mc.backward()
@@ -303,35 +334,39 @@ def test_matmul_backward():
     assert np.allclose(mb.grad, tb.grad.numpy())
 
 
-def test_batched_matmul_foward():
-    batch_size = 16
-    ma = Tensor(np.random.randn(batch_size, 2))
-    mb = Tensor(np.random.randn(2, 2))
-    mc = ma.matmul(mb)
+def test_batched_matmul_forward():
+    q = np.random.randn(1, 2, 3, 4)
+    k = q.swapaxes(-1, -2)
 
-    ta = torch.from_numpy(ma.data)
-    tb = torch.from_numpy(mb.data)
-    tc = ta @ tb
+    my_q = Tensor(q)
+    my_k = Tensor(k)
+    my_out = my_q.matmul(my_k)
 
-    assert np.allclose(mc.data, tc.numpy())
+    torch_q = torch.from_numpy(q)
+    torch_k = torch.from_numpy(k)
+    torch_out = torch_q @ torch_k
+
+    assert np.allclose(my_out.data, torch_out.detach().numpy())
 
 
 def test_batched_matmul_backward():
-    batch_size = 16
-    ma = Tensor(np.random.randn(batch_size, 2))
-    mb = Tensor(np.random.randn(2, 2))
-    mc = ma.matmul(mb)
-    mc.backward()
+    q = np.random.randn(1, 2, 3, 4)
+    k = q.swapaxes(-1, -2)
 
-    ta = torch.from_numpy(ma.data)
-    ta.requires_grad = True
-    tb = torch.from_numpy(mb.data)
-    tb.requires_grad = True
-    tc = ta @ tb
-    tc.backward(gradient=torch.ones_like(tc))
+    my_q = Tensor(q)
+    my_k = Tensor(k)
+    my_out = my_q.matmul(my_k)
+    my_out.backward()
 
-    assert np.allclose(ma.grad, ta.grad.numpy())
-    assert np.allclose(mb.grad, tb.grad.numpy())
+    torch_q = torch.from_numpy(q)
+    torch_q.requires_grad = True
+    torch_k = torch.from_numpy(k)
+    torch_k.requires_grad = True
+    torch_out = torch_q @ torch_k
+    torch_out.backward(gradient=torch.ones_like(torch_out))
+
+    assert np.allclose(my_q.grad, torch_q.grad.numpy())
+    assert np.allclose(my_k.grad, torch_k.grad.numpy())
 
 
 def test_relu_forward():
@@ -449,42 +484,79 @@ def test_softmax_backward_vector():
 
     my_x = Tensor(x)
     my_out = my_x.softmax(dim=dim)
-    my_out.backward()
+    dout = np.random.randn(*my_out.shape)
+    my_out.backward(gradient=dout)
 
     torch_x = torch.as_tensor(x)
     torch_x.requires_grad = True
     torch_out = torch_x.softmax(dim=dim)
-    torch_out.backward(gradient=torch.ones_like(torch_out))
+    torch_out.backward(gradient=torch.as_tensor(dout))
     assert np.allclose(my_x.grad, torch_x.grad.numpy())
 
 
 def test_softmax_backward_matrix():
-    x = np.random.randn(5, 5)
-    dim = 0
-
-    my_x = Tensor(x)
-    my_out = my_x.softmax(dim=dim)
-    my_out.backward()
-
-    torch_x = torch.as_tensor(x)
-    torch_x.requires_grad = True
-    torch_out = torch_x.softmax(dim=dim)
-    torch_out.backward(gradient=torch.ones_like(torch_out))
-    assert np.allclose(my_x.grad, torch_x.grad.numpy())
-
-
-def test_softmax_backward_tensor():
-    x = np.random.randn(1, 2, 5, 5)
+    x = np.random.randn(3, 5)
     dim = -1
 
     my_x = Tensor(x)
     my_out = my_x.softmax(dim=dim)
-    my_out.backward()
+    dout = np.random.randn(*my_out.shape)
+    my_out.backward(gradient=dout)
 
     torch_x = torch.as_tensor(x)
     torch_x.requires_grad = True
     torch_out = torch_x.softmax(dim=dim)
-    torch_out.backward(gradient=torch.ones_like(torch_out))
+    torch_out.backward(gradient=torch.as_tensor(dout))
+    assert np.allclose(my_x.grad, torch_x.grad.numpy())
+
+
+def test_softmax_backward_matrix_dim_0():
+    x = np.random.randn(3, 5)
+    dim = 0
+
+    my_x = Tensor(x)
+    my_out = my_x.softmax(dim=dim)
+    dout = np.random.randn(*my_out.shape)
+    my_out.backward(gradient=dout)
+
+    torch_x = torch.as_tensor(x)
+    torch_x.requires_grad = True
+    torch_out = torch_x.softmax(dim=dim)
+    torch_out.backward(gradient=torch.as_tensor(dout))
+    assert np.allclose(my_x.grad, torch_x.grad.numpy())
+
+
+def test_softmax_backward_tensor():
+    x = np.random.randn(8, 2, 5, 5)
+    dim = -1
+
+    my_x = Tensor(x)
+    my_out = my_x.softmax(dim=dim)
+
+    dout = np.random.randn(*my_out.shape)
+    my_out.backward(gradient=dout)
+
+    torch_x = torch.as_tensor(x)
+    torch_x.requires_grad = True
+    torch_out = torch_x.softmax(dim=dim)
+    torch_out.backward(gradient=torch.as_tensor(dout))
+    assert np.allclose(my_x.grad, torch_x.grad.numpy())
+
+
+def test_softmax_backward_tensor_dim_2():
+    x = np.random.randn(8, 2, 5, 5)
+    dim = 2
+
+    my_x = Tensor(x)
+    my_out = my_x.softmax(dim=dim)
+
+    dout = np.random.randn(*my_out.shape)
+    my_out.backward(gradient=dout)
+
+    torch_x = torch.as_tensor(x)
+    torch_x.requires_grad = True
+    torch_out = torch_x.softmax(dim=dim)
+    torch_out.backward(gradient=torch.as_tensor(dout))
     assert np.allclose(my_x.grad, torch_x.grad.numpy())
 
 

test/test_functional.py
@@ -41,6 +41,23 @@ def test_batched_cross_entropy_forward():
     assert np.allclose(mout.data, tout.numpy())
 
 
+def test_batched_cross_entropy_ignore_idx_forward():
+    batch_size = 2
+    a = np.random.randn(batch_size, 5)
+    target = np.array([1, -1], dtype=int)
+
+    my_a = Tensor(a)
+    my_target = Tensor(target)
+    my_out = F.batched_cross_entropy(my_a, my_target, ignore_index=-1)
+
+    torch_a = torch.as_tensor(a)
+    torch_target = torch.as_tensor(target)
+    torch_out = torch.nn.functional.cross_entropy(
+        torch_a, torch_target, ignore_index=-1
+    )
+    assert np.allclose(my_out.data, torch_out.numpy())
+
+
 def test_batched_cross_entropy_backward():
     batch_size = 2
     ma = Tensor(np.random.randn(batch_size, 5))
@@ -54,6 +71,26 @@ def test_batched_cross_entropy_backward():
     tout = torch.nn.functional.cross_entropy(ta, ttarget)
     tout.backward(gradient=torch.ones_like(tout))
     assert np.allclose(ma.grad, ta.grad.numpy())
+
+
+def test_batched_cross_entropy_ignore_idx_backward():
+    batch_size = 2
+    a = np.random.randn(batch_size, 5)
+    target = np.array([1, -1], dtype=int)
+
+    my_a = Tensor(a)
+    my_target = Tensor(target)
+    my_out = F.batched_cross_entropy(my_a, my_target, ignore_index=-1)
+    my_out.backward()
+
+    torch_a = torch.as_tensor(a)
+    torch_a.requires_grad = True
+    torch_target = torch.as_tensor(target)
+    torch_out = torch.nn.functional.cross_entropy(
+        torch_a, torch_target, ignore_index=-1
+    )
+    torch_out.backward(gradient=torch.ones_like(torch_out))
+    assert np.allclose(my_a.grad, torch_a.grad.numpy())
 
 
 # helper function to test conv
@@ -358,11 +395,13 @@ def test_embedding_backward():
     my_input = Tensor(input)
     my_weight = Tensor(weight)
     my_out = F.embedding(my_input, my_weight)
-    my_out.backward()
+    dout = np.random.randn(*my_out.shape)
+    my_out.backward(gradient=dout)
 
     torch_input = torch.as_tensor(input)
     torch_weight = torch.as_tensor(weight)
     torch_weight.requires_grad = True
     torch_out = torch.nn.functional.embedding(torch_input, torch_weight)
-    torch_out.backward(gradient=torch.ones_like(torch_out))
+    torch_out.backward(gradient=torch.as_tensor(dout))
+
     assert np.allclose(my_weight.grad, torch_weight.grad.numpy())