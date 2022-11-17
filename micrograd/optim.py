3b75a4da5cbe7544d9245e0293cae3f63f2bcd27
ckkissane <67170576+ckkissane@users.noreply.github.com>
2022-11-16 19:33:52 -0500

    Adam Optimizer

--- micrograd/optim.py
+++ micrograd/optim.py
@@ -18,3 +18,32 @@ class SGD:
     def step(self):
         for p in self.params:
             p.data -= self.lr * p.grad
+
+
+class Adam:
+    def __init__(self, params: List[Tensor], lr=0.001, betas=(0.9, 0.999), eps=1e-8):
+        self.params = params
+        self.lr = lr
+
+        self.t = 0
+        self.beta1, self.beta2 = betas
+        self.eps = eps
+        self.m = [np.zeros_like(p.data) for p in params]
+        self.v = [np.zeros_like(p.data) for p in params]
+
+    def __repr__(self):
+        return f"Adam(lr={self.lr})"
+
+    def zero_grad(self):
+        for p in self.params:
+            p.grad = np.zeros_like(p.grad)
+
+    def step(self):
+        self.t += 1
+        for i, p in enumerate(self.params):
+            g = p.grad
+            self.m[i] = self.beta1 * self.m[i] + (1.0 - self.beta1) * g
+            self.v[i] = self.beta2 * self.v[i] + (1.0 - self.beta2) * g**2
+            m_hat = self.m[i] / (1.0 - self.beta1**self.t)
+            v_hat = self.v[i] / (1.0 - self.beta2**self.t)
+            p.data -= self.lr * m_hat / np.sqrt(v_hat + self.eps)
--- test/test_optim.py
+++ test/test_optim.py
@@ -22,14 +22,50 @@ def test_vanilla_sgd():
     torch_inp.requires_grad = True
 
     for _ in range(num_iters):
+        my_sgd.zero_grad()
         my_out = my_lin(my_inp)
         my_out.backward()
         my_sgd.step()
 
     for _ in range(num_iters):
+        torch_sgd.zero_grad()
         torch_out = torch_lin(torch_inp)
         torch_out.backward(gradient=torch.ones_like(torch_out))
         torch_sgd.step()
+
+    assert np.allclose(my_lin.bias.data, torch_lin.bias.data.detach().numpy())
+    assert np.allclose(my_lin.weight.data, torch_lin.weight.data.T.detach().numpy())
+
+
+def test_adam():
+    inp = np.random.randn(10)
+    lr = 0.1
+    num_iters = 5
+    betas = (0.9, 0.999)
+    eps = 1e-8
+
+    my_lin = nn.Linear(10, 20)
+    my_adam = optim.Adam(my_lin.parameters(), lr=lr, betas=betas, eps=eps)
+    my_inp = Tensor(inp)
+
+    torch_lin = torch.nn.Linear(10, 20)
+    torch_lin.bias = torch.nn.Parameter(torch.from_numpy(my_lin.bias.data))
+    torch_lin.weight = torch.nn.Parameter(torch.from_numpy(my_lin.weight.data.T))
+    torch_adam = torch.optim.Adam(torch_lin.parameters(), lr=lr, betas=betas, eps=eps)
+    torch_inp = torch.as_tensor(inp)
+    torch_inp.requires_grad = True
+
+    for _ in range(num_iters):
+        my_adam.zero_grad()
+        my_out = my_lin(my_inp)
+        my_out.backward()
+        my_adam.step()
+
+    for _ in range(num_iters):
+        torch_adam.zero_grad()
+        torch_out = torch_lin(torch_inp)
+        torch_out.backward(gradient=torch.ones_like(torch_out))
+        torch_adam.step()
 
     assert np.allclose(my_lin.bias.data, torch_lin.bias.data.detach().numpy())
     assert np.allclose(my_lin.weight.data, torch_lin.weight.data.T.detach().numpy())
